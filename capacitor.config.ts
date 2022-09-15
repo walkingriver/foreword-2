97c3f21b6528a5c1b30a2b94b718e1198ed49875
Michael Callaghan <mikecallaghan67@gmail.com>
2022-09-14 22:58:53 -0400

    Home Page Done

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.walkingriver.foreword',
   appName: 'foreword-2',
   webDir: 'www',
   bundledWebRuntime: false
