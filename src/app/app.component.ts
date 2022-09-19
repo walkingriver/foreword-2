8ce74d2dcdc3dff0500986038e5bc315a8662f12
Michael Callaghan <mikecallaghan67@gmail.com>
2022-09-18 21:56:24 -0400

    Fix layout for multiple device resolutions (#1)
    * Landscape layout is working well
    
    * A few extra style tweaks for landscape
    
    * Layout WIP
    
    * Most landscape resolutions working
    
    * Layout issues mostly solved.
    
    * Added polyfill for mobile drag/drop
    
    * 3x3 layout is good for all devices
    
    * 3x3 are working better
    
    * Update version number
    
    * Minor format change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { polyfill } from 'mobile-drag-drop';
+import { scrollBehaviourDragImageTranslateOverride } from 'mobile-drag-drop/scroll-behaviour';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,45 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+
+  ) {
+    this.platform.ready().then(() => {
+      this.initializeApp();
+    });
+  }
+
+  initializeApp() {
+    // this.statusBar.styleDefault();
+    // this.statusBar.overlaysWebView(false);
+    // this.statusBar.backgroundColorByHexString('#C1A172');
+    // this.splashScreen.hide();
+
+    // Test via a getter in the options object to see if the passive property is accessed
+    let supportsPassive = false;
+    try {
+      const opts = Object.defineProperty({}, 'passive', {
+        get() {
+          supportsPassive = true;
+        }
+      });
+      window.addEventListener('testPassive', null, opts);
+      window.removeEventListener('testPassive', null, opts);
+    } catch (e) { }
+
+    // workaround to make scroll prevent work in iOS Safari > 10
+    try {
+      const isPolyfill = polyfill({
+        // use this to make use of the scroll behaviour
+        dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride
+      });
+      console.log('IsPolyFill: ', isPolyfill);
+      if (supportsPassive) {
+        window.addEventListener('touchmove', () => { }, { passive: false });
+      } else {
+        window.addEventListener('touchmove', () => { });
+      }
+    } catch (e) { }
+  }
 }
